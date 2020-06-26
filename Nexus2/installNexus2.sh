961b79ca75500497c26e6818255e5ef7c9f3808f
Daniel Díez <jddiez@live.com>
2020-06-26 13:54:52 +0200

    Nexus2 added and checked v0.0.2

--- Nexus2/installNexus2.sh
+++ Nexus2/installNexus2.sh
@@ -13,11 +13,20 @@ sudo yum install epel-release -y &> /home/vagrant/epel.txt
 sudo yum install wget net-tools java-1.8.0-openjdk.x86_64 -y
 sudo mkdir /home/vagrant/installer
 cd /home/vagrant/installer
-sudo wget https://download.sonatype.com/nexus/professional-bundle/nexus-professional-2.14.8-01-bundle.tar.gz --no-check-certificate
-sudo tar -xvf * -C /opt
+echo "******** Comienza el proceso de instalación de Nexus 2 **************"
+echo "STEP 1: Descarga de Nexus 2.14.08-01"
+sudo wget -q https://download.sonatype.com/nexus/oss/nexus-2.14.8-01-bundle.tar.gz --no-check-certificate
+echo "STEP 2: Descompresión en /opt"
+sudo tar -xf * -C /opt
+echo "STEP 3: Adición usuario nexus y filesystem propietario"
 sudo useradd nexus
-sudo chown -R nexus:nexus /opt/nexus-proffesional-2.14.8-01/
-sudo sed -i 's/#RUN_AS_USER=/RUN_AS_USER=nexus/g' /opt/nexus-professional-2.14.8-01/bin/nexus
-sudo ln -s /opt/nexus-professional-2.14.8-01/bin/nexus /etc/init.d/nexus
+sudo chown -R nexus:nexus /opt/nexus-2.14.8-01/
+echo "STEP 4: Modificación de fichero de configuración y enlance simbólico a init.d"
+sudo sed -i 's/#RUN_AS_USER=/RUN_AS_USER=nexus/g' /opt/nexus-2.14.8-01/bin/nexus
+sudo ln -s /opt/nexus-2.14.8-01/bin/nexus /etc/init.d/nexus
+echo "STEP 5: Habilitar Nexus para inicio e iniciar."
 sudo systemctl enable nexus
 sudo systemctl start nexus
+echo "FIN ===================================================================================="
+echo "Puedes acceder a http://$eth1ip:8081/nexus"
+echo "User: admin / Password: admin123"
